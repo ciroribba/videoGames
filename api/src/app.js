3321c84485409dfc90ccf8148e4e7b38c8ab98d8
ciroribba <ciro.ribba@gmail.com>
2021-09-04 19:02:07 -0300

    llevo todo a variables de entorno

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,9 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const {
+  CLIENT_URL
+} = process.env;
 
 
 require('./db.js');
@@ -16,7 +19,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://videogames-ciro.netlify.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', CLIENT_URL); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
