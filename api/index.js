49a69681db67bd12029153d547b83dd6b28116d1
ciroribba <ciro.ribba@gmail.com>
2021-09-04 16:18:28 -0300

    fix genres

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn, Genre } = require('./src/db.js');
-const{ getAllGenresFromApi } = require('../api/src/controllers/genres')
+const{ getAllGenresFromApi } = require('./src/controllers/genres')
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then( async () => {
